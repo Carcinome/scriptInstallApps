7d8d45e5c9a217ff010fff3f120e9aeccb997d65
Carcinome <carcinome.work@gmail.com>
2026-07-02 15:33:26 +0200

    Ajout de la fonction find_rpms

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob # Pour développer le motif *.rpm en liste de fichiers.
-import os   # Pour assembler proprement le chemin (dossier + motif).
+import glob     # Pour développer le motif *.rpm en liste de fichiers.
+import os       # Pour assembler proprement le chemin (dossier + motif).
+import hashlib  # Boîte à outils de hashing, livrée avec Python.
 
 
 def find_rpms(rpm_dir):
@@ -14,6 +15,8 @@ def find_rpms(rpm_dir):
     fichiers = glob.glob(motif)
 
     # Garde-fou : le dossier existe, mais il est peut-être vide.
+    if not fichiers:
+        raise FileNotFoundError ("Le dossier est vide:" + rpm_dir)
 
     # On renvoie cette liste à celui qui a appelé la fonction.
     return fichiers
@@ -22,5 +25,11 @@ def find_rpms(rpm_dir):
 resultat = find_rpms("C:/testrpms")
 print(resultat)
 
+# On ouvre le fichier en mode binaire pour lire ses octets bruts. rb = 'read binary'.
+with open("C:/testrpms/paquet1.rpm", "rb") as f: # With pour fermer le fichier automatiquement.
+    contenu = f.read() # Aspire tout le contenu du fichier en octets.
 
-
+# Calcul de l'empreinte en hexadécimal.
+empreinte = hashlib.sha256(contenu).hexdigest() # Passe ces octets dans la moulinette sha256
+# et .hexdigest() sort le résultat en texte hexadécimal lisible.
+print(empreinte)
